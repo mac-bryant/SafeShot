7b827165ffb0d6691a982ac1720c4b763b4a5659
Mackenzie Bryant <mackenziebryantk@gmail.com>
2020-05-01 12:31:14 -0500

    Initial Commit

--- SafeShot/ContentView.swift
+++ SafeShot/ContentView.swift
@@ -6,16 +6,61 @@
 //  Copyright © 2020 Mackenzie Bryant. All rights reserved.
 //
 
+import Combine
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
+    
+    var selectPlatforms = ["Instagram", "Facebook", "TikTok", "YouTube"]
+    @State private var selectedMode = 0
+    @State var reportUserName: String = ""
+    
+    var isValid: Bool {
+        if reportUserName.isEmpty {
+            return false
+        }
+        return true
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section(header: Text("Select Platform")){
+                    Picker(selection: $selectedMode, label: Text("Platforms")) {
+                        ForEach(0..<selectPlatforms.count) {
+                            Text(self.selectPlatforms[$0])
+                        }
+                    }.padding()
+                }
+                Section(header: Text("Please report the Username that has violated your safety online.")) {
+                    HStack {
+                        TextField("Username", text: $reportUserName)
+                    }.padding()
+                    
+                }
+                Section(header: Text("We strongly encourage you to share any photos that have violated your safety online.")) {
+                    HStack {
+                        Button(action: {
+                                self.attachPhotos()
+                            }) {
+                                Text("Attach Photos")
+                        }
+                    }.padding()
+                }
+                Section {
+                    Button(action: {
+                        self.submitReport()
+                    }) {
+                        Text("Submit Report")
+                }.padding()
+            }
+            } .navigationBarTitle("SafeShot")
+        }
+    }
+    func attachPhotos() {
+        
+    }
+    func submitReport() {
+        
     }
 }
